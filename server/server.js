e7a4ef1da8d8f28c4111dec74bcf85dd6cbdad9d
Eric <ehannum@gmail.com>
2014-03-05 05:36:06 -0800

    [wip] sockate storage object refactored

--- server/server.js
+++ server/server.js
@@ -1,22 +1,64 @@
 var net = require('net');
 
-var openSockets = [];
+var channels = {};
 
-var broadcast = function (socket, message) {
-  for (var i = 0; i < openSockets.length; i++) {
-    if (openSockets[i] !== socket) {
-      openSockets[i].write('<= Anonymous: ' + message);
+/* -- channel list format
+  {
+    channel1: {
+      userName1: socket1,
+      userName2: socket2
+    },
+    channel2: {
+      ...
     }
-  };
+  }
+*/
+
+// var users = {};
+
+/* -- user list format
+  {
+    username1: {
+      socket: socket,
+      channel: channelName
+    },
+    username2: {
+      ...
+    }
+  }
+*/
+
+var broadcast = function (socket, message, channel) {
+  if (message) {
+    for (user in channels[channel]) {
+        channels[channel][user].write('<= ' + user + ': ' + message);
+    }
+  }
 };
 
 var requestHandler = function (socket) {
-  openSockets.push(socket);
-  broadcast('Welcome to Super Best Buds Chat!');
-  broadcast('Login name?');
+  socket.write('<= Welcome to Super Best Buds Chat!\r\n<= Login name?\r\n');
+  console.log(socket);
+
+  // -- User Input
+
   socket.on('data', function (data) {
-    broadcast(socket, data);
+    if (data) {
+      broadcast(socket, data);
+    }
   });
+
+  socket.on('end', function () {
+    // delete references to user in users and channels
+    // broadcast "user has left the channel" message
+  });
+};
+
+var joinChannel = function (socket, username, channel) {
+  if (!channels[channel]) {
+    channels[channel] = {};
+  }
+  channels[channel][username] = socket;
 };
 
 var server = net.createServer(requestHandler).listen(8080);
